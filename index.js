40c3a549ba1d79eb6e67f25f86ff8dfb218b2739
GeetikaKaliaa <geetika.kalia51@gmail.com>
2023-10-02 00:17:58 +0530

    made changes done

--- index.js
+++ index.js
@@ -1,54 +1,3 @@
-var ml4 = {};
-ml4.opacityIn = [0,1];
-ml4.scaleIn = [0.2, 1];
-ml4.scaleOut = 3;
-ml4.durationIn = 800;
-ml4.durationOut = 600;
-ml4.delay = 500;
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml4 .letters-1',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-1',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4',
-    opacity: 0,
-    duration: 500,
-    delay: 500
-  });
   jQuery(document).ready(function(){
 
     function detect_active(){
@@ -57,36 +6,36 @@ anime.timeline({loop: true})
         $("#dp-dots li").removeClass("active");
         $("#dp-dots li[data-class="+ get_active +"]").addClass("active");
       }
-      $("#dp-next").click(function(){
-        var total = $(".dp_item").length;
-        $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
-        $.each($('.dp_item'), function (index, dp_item) {
-          $(dp_item).attr('data-position', index + 1);
-        });
-        detect_active();
+      // $("#dp-next").click(function(){
+      //   var total = $(".dp_item").length;
+      //   $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+      //   $.each($('.dp_item'), function (index, dp_item) {
+      //     $(dp_item).attr('data-position', index + 1);
+      //   });
+      //   detect_active();
     
-      });
+      // });
     
-      $("#dp-prev").click(function(){
-        var total = $(".dp_item").length;
-        $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
-        $.each($('.dp_item'), function (index, dp_item) {
-          $(dp_item).attr('data-position', index + 1);
-        });
+      // $("#dp-prev").click(function(){
+      //   var total = $(".dp_item").length;
+      //   $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+      //   $.each($('.dp_item'), function (index, dp_item) {
+      //     $(dp_item).attr('data-position', index + 1);
+      //   });
     
-        detect_active();
-      });
+      //   detect_active();
+      // });
     
-      $("#dp-dots li").click(function(){
-        $("#dp-dots li").removeClass("active");
-        $(this).addClass("active");
-        var get_slide = $(this).attr('data-class');
-        console.log(get_slide);
-        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
-        $.each($('.dp_item'), function (index, dp_item) {
-          $(dp_item).attr('data-position', index + 1);
-        });
-      });
+      // $("#dp-dots li").click(function(){
+      //   $("#dp-dots li").removeClass("active");
+      //   $(this).addClass("active");
+      //   var get_slide = $(this).attr('data-class');
+      //   console.log(get_slide);
+      //   $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+      //   $.each($('.dp_item'), function (index, dp_item) {
+      //     $(dp_item).attr('data-position', index + 1);
+      //   });
+      // });
     
     
       $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function(){
